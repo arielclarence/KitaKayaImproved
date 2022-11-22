f74ab10cc0624fd4dc3b4a7b7e975569b852f4c9
calvinadhikang <calvinadhikang@gmail.com>
2022-11-22 23:15:33 +0700

    fix: table user column name

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('user', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nama');
             $table->string('email')->unique();
             $table->string('password');
             $table->string('nama');
