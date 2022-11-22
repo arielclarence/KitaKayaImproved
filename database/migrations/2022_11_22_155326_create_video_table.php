dd9af55e08c31b4ff9a9da41a212ff61cccef752
calvinadhikang <calvinadhikang@gmail.com>
2022-11-22 22:55:15 +0700

    feat #15

---
+++ database/migrations/2022_11_22_155326_create_video_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('video', function (Blueprint $table) {
+            $table->id();
+            $table->text('nama');
+            $table->text('location');
+            $table->integer('bab');
+            $table->boolean('vip');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('video');
+    }
+};
