15e40448448b1702f4ad384635caaea922254c96
calvinadhikang <calvinadhikang@gmail.com>
2022-11-22 23:13:28 +0700

    fix: user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,13 +13,15 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('user', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->string('nama');
+            $table->integer('umur');
+            $table->integer('role');
+            $table->integer('status');
             $table->timestamps();
         });
     }
