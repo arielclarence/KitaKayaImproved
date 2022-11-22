d5093deb9cf4072d5fb333ab7bf0c43109366e70
calvinadhikang <calvinadhikang@gmail.com>
2022-11-22 22:44:53 +0700

    feat #9

---
+++ database/migrations/2022_11_22_154350_create_completion_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('completion', function (Blueprint $table) {
+            $table->id('bab');
+            $table->integer('member');
+            $table->float('completion');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('completion');
+    }
+};
