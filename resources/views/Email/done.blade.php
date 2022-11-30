dcdc6f1c53c106976e3f6f87ac940a384e833011
calvinadhikang <calvinadhikang@gmail.com>
2022-11-30 11:18:21 +0700

    fixing email verification view

--- resources/views/Email/done.blade.php
+++ resources/views/Email/done.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     <style>
         body{
             background-color: #8EC5FC;
@@ -13,10 +15,12 @@
     </style>
 </head>
 <body>
-    <div class="container">
-        <h1>Email verified!</h1>
-        <h3>Redirecting you to main page...</h3>
-        <h5>Click here if you're not redirected after 3 seconds</h5>
+    <div class="container d-flex justify-content-center" style="height: 100vh">
+        <div class="align-self-center bg-white rounded shadow p-5">
+            <h1>Email verified!</h1>
+            <h3>Redirecting you to main page...</h3>
+            <h5>Click here if you're not redirected after 3 seconds</h5>
+        </div>
     </div>
 </body>
 </html>
